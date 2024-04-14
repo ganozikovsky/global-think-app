2e3393e2a3bbff464b71220580cfe463937081a1
Gonzalo Agustín Nozikovsky <103380329+ganozikovsky@users.noreply.github.com>
2024-04-14 11:32:37 -0300

    fix(user): api property in update entity

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { IsBoolean, IsOptional } from 'class-validator';
 
 import { CreateUserDto } from './create-user.dto';
@@ -13,5 +13,6 @@ import { CreateUserDto } from './create-user.dto';
 export class UpdateUserDto extends PartialType(CreateUserDto) {
   @IsBoolean()
   @IsOptional()
+  @ApiProperty({ example: true })
   state?: boolean;
 }
